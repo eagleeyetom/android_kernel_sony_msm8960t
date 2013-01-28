accc3bec9c6714b1faedb4b0b670febff929befe
Linux Build Service Account <lnxbuild@localhost>
2013-01-28 08:25:48 -0800

    Merge "msm_fb: display: add wfd clk ctrl in pipe_commit" Conflicts: 	arch/arm/mach-msm/board-8960.c 	drivers/char/diag/diag_debugfs.c 	drivers/char/diag/diag_masks.c 	drivers/char/diag/diagchar_core.c 	drivers/media/video/msm_wfd/mdp-4-subdev.c

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -1721,6 +1721,11 @@ static uint8_t spm_retention_cmd_sequence[] __initdata = {
 			0x0B, 0x00, 0x0f,
 };
 
+static uint8_t spm_retention_cmd_sequence[] __initdata = {
+			0x00, 0x05, 0x03, 0x0D,
+			0x0B, 0x00, 0x0f,
+};
+
 static uint8_t spm_power_collapse_without_rpm[] __initdata = {
 	0x00, 0x24, 0x54, 0x10,
 	0x09, 0x03, 0x01,
@@ -1742,6 +1747,32 @@ static uint8_t spm_power_collapse_without_rpm_krait_v3[] __initdata = {
 	0x10, 0x84, 0x30, 0x0C,
 	0x24, 0x30, 0x0f,
 };
+
+static struct msm_spm_seq_entry msm_spm_boot_cpu_seq_list[] __initdata = {
+	[0] = {
+		.mode = MSM_SPM_MODE_CLOCK_GATING,
+		.notify_rpm = false,
+		.cmd = spm_wfi_cmd_sequence,
+	},
+
+	[1] = {
+		.mode = MSM_SPM_MODE_POWER_RETENTION,
+		.notify_rpm = false,
+		.cmd = spm_retention_cmd_sequence,
+	},
+
+	[2] = {
+		.mode = MSM_SPM_MODE_POWER_COLLAPSE,
+		.notify_rpm = false,
+		.cmd = spm_power_collapse_without_rpm,
+	},
+	[3] = {
+		.mode = MSM_SPM_MODE_POWER_COLLAPSE,
+		.notify_rpm = true,
+		.cmd = spm_power_collapse_with_rpm,
+	},
+};
+
 
 static struct msm_spm_seq_entry msm_spm_boot_cpu_seq_list[] __initdata = {
 	[0] = {
