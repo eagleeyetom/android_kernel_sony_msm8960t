4bda10ce31284f980ad535c3de47a90d96a75fb0
Linux Build Service Account <lnxbuild@localhost>
2013-01-29 16:54:35 -0800

    Merge "msm: power: update power drivers to msm-3.4 tip" Conflicts: 	arch/arm/mach-msm/board-8960.c 	drivers/char/diag/diag_debugfs.c 	drivers/char/diag/diag_masks.c 	drivers/char/diag/diagchar_core.c 	drivers/video/msm/mipi_lgit_video_wxga_pt.c

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -1904,6 +1904,21 @@ static uint8_t spm_power_collapse_with_rpm_krait_v3[] __initdata = {
 	0x24, 0x30, 0x0f,
 };
 
+/* 8960AB has a different command to assert apc_pdn */
+static uint8_t spm_power_collapse_without_rpm_krait_v3[] __initdata = {
+	0x00, 0x24, 0x84, 0x10,
+	0x09, 0x03, 0x01,
+	0x10, 0x84, 0x30, 0x0C,
+	0x24, 0x30, 0x0f,
+};
+
+static uint8_t spm_power_collapse_with_rpm_krait_v3[] __initdata = {
+	0x00, 0x24, 0x84, 0x10,
+	0x09, 0x07, 0x01, 0x0B,
+	0x10, 0x84, 0x30, 0x0C,
+	0x24, 0x30, 0x0f,
+};
+
 static struct msm_spm_seq_entry msm_spm_boot_cpu_seq_list[] __initdata = {
 	[0] = {
 		.mode = MSM_SPM_MODE_CLOCK_GATING,
