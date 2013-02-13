414ce222d1249e85c074fe6a2614b3514519ab19
Tarun Karra <tkarra@codeaurora.org>
2013-02-13 13:57:25 -0800

    msm: kgsl: Allow pagefault policy to be controlled by userspace
    GPU pagefault policy can be set to below options:
       a) enable/disable GPU HALT on pagefaults
       b) log only one pagefault per page.
    GPU pagefault policy can be controled using panel file
    and debugfs.
    
    Change-Id: I0613e0d087fc45eb5e519e1c834a33f32376672f
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/kgsl_iommu.c

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -108,7 +108,8 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 	struct kgsl_iommu_device *iommu_dev;
 	unsigned int ptbase, fsr;
 	struct kgsl_device *device;
-	unsigned int no_page_fault_log;
+	struct adreno_device *adreno_dev;
+	unsigned int no_page_fault_log = 0;
 
 	ret = get_iommu_unit(dev, &mmu, &iommu_unit);
 	if (ret)
@@ -121,6 +122,7 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 	}
 	iommu = mmu->priv;
 	device = mmu->device;
+	adreno_dev = ADRENO_DEVICE(device);
 
 	ptbase = KGSL_IOMMU_GET_CTX_REG(iommu, iommu_unit,
 					iommu_dev->ctx_id, TTBR0);
@@ -128,7 +130,8 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 	fsr = KGSL_IOMMU_GET_CTX_REG(iommu, iommu_unit,
 		iommu_dev->ctx_id, FSR);
 
-	no_page_fault_log = kgsl_mmu_log_fault_addr(mmu, ptbase, addr);
+	if (adreno_dev->ft_pf_policy & KGSL_FT_PAGEFAULT_LOG_ONE_PER_PAGE)
+		no_page_fault_log = kgsl_mmu_log_fault_addr(mmu, ptbase, addr);
 
 	if (!no_page_fault_log) {
 		KGSL_MEM_CRIT(iommu_dev->kgsldev,
@@ -141,6 +144,14 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 	trace_kgsl_mmu_pagefault(iommu_dev->kgsldev, addr,
 			kgsl_mmu_get_ptname_from_ptbase(mmu, ptbase), 0);
 
+	/*
+	 * We do not want the h/w to resume fetching data from an iommu unit
+	 * that has faulted, this is better for debugging as it will stall
+	 * the GPU and trigger a snapshot. To stall the transaction return
+	 * EBUSY error.
+	 */
+	if (adreno_dev->ft_pf_policy & KGSL_FT_PAGEFAULT_GPUHALT_ENABLE)
+		ret = -EBUSY;
 done:
 	return ret;
 }
