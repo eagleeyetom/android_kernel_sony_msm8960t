d3fe201ef3de1ae2c99984fb669da09bcdff29fd
Pavankumar Kondeti <pkondeti@codeaurora.org>
2013-04-25 10:35:54 +0530

    USB: android: Fix kernel panic during ACM enable
    92fe88c8 commit (usb: gadget: Support multiple HSIC transport
    instances for dun and rmnet) introduced a sysfs file called
    "transport_names" for DUN and RMNET to accept transport names
    from user space.  The transport names are used for registering
    platform drivers.  Update ACM function too.  Otherwise NULL
    string is used to register platform driver and kernel panics.
    
    CRs-Fixed: 479434
    Change-Id: I88b56f252f74bcfb2e664a6e37b64fe224739381
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    
    Conflicts:
    	drivers/usb/gadget/f_acm.c

--- drivers/usb/gadget/f_acm.c
+++ drivers/usb/gadget/f_acm.c
@@ -940,7 +940,7 @@ int acm_bind_config(struct usb_configuration *c, u8 port_num)
 /**
  * acm_init_port - bind a acm_port to its transport
  */
-static int acm_init_port(int port_num, const char *name)
+static int acm_init_port(int port_num, const char *name, const char *port_name)
 {
 	enum transport_type transport;
 
@@ -966,6 +966,13 @@ static int acm_init_port(int port_num, const char *name)
 	case USB_GADGET_XPORT_SMD:
 		gacm_ports[port_num].client_port_num = no_acm_smd_ports;
 		no_acm_smd_ports++;
+		break;
+	case USB_GADGET_XPORT_HSIC:
+		ghsic_ctrl_set_port_name(port_name, name);
+		ghsic_data_set_port_name(port_name, name);
+
+		/*client port number will be updated in acm_port_setup*/
+		no_acm_hsic_sports++;
 		break;
 	default:
 		pr_err("%s: Un-supported transport transport: %u\n",
